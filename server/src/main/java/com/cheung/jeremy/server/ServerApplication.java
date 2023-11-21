33e5413069ebbac8c33602b64837900f69d45a2b
Jeremy Cheung <efyjc12@nottingham.ac.uk>
2023-11-21 02:41:15 +0000

    Add student class

--- server/src/main/java/com/cheung/jeremy/server/ServerApplication.java
+++ server/src/main/java/com/cheung/jeremy/server/ServerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cheung.jeremy.server.student.Student;
+
+import java.time.Month;
+import java.time.LocalDate;
 import java.util.*;
 
 @SpringBootApplication
@@ -15,8 +20,14 @@ public class ServerApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello","World");
+	public List<Student> hello() {
+		return List.of(new Student(
+			1L,
+			"Gen",
+			"gen@gmail.com",
+			LocalDate.of(2000, Month.JANUARY, 5),
+			21
+		));
 	}
 
 }
