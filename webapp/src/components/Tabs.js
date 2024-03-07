34ee8edf92a60eff762655f6d1124abf16aa66f7
Jeremy-Cheung <jeremycheung@hotmail.co.uk>
2024-03-07 19:00:29 +0000

    Add button component to tabs

--- webapp/src/components/Tabs.js
+++ webapp/src/components/Tabs.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import classes from "./Tabs.module.css";
+import Button from "./Button.js";
 function Tabs(props) {
+
+  const GET_BUTTON = {
+    name: "GET",
+    action: null,
+  };
+
+  const POST_BUTTON = {
+    name: "POST",
+    action: null,
+  };
+
+  const PUT_BUTTON = {
+    name: "PUT",
+    action: null,
+  };
+
+  const DELETE_BUTTON = {
+    name: "DELETE",
+    action: null,
+  };
+
+  
+
   return (
     <div>
       <div className={props.state === 1 ? classes.activeTab : classes.inactiveTab}>
-        tab 1
+        <Button param={GET_BUTTON}></Button>
       </div>
       <div className={props.state === 2 ? classes.activeTab : classes.inactiveTab}>
-        tab 2
+        <Button param={POST_BUTTON}></Button>
       </div>
       <div className={props.state === 3 ? classes.activeTab : classes.inactiveTab}>
-        tab 3
+        <Button param={PUT_BUTTON}></Button>
       </div>
       <div className={props.state === 4 ? classes.activeTab : classes.inactiveTab}>
-        tab 4
+        <Button param={DELETE_BUTTON}></Button>
       </div>
     </div>
   );
